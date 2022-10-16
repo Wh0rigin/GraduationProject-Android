e26d52280b341f5d2a0837caaa66297d8adfd63d
Lightsake-Sensei <805935229@qq.com>
2022-10-17 00:29:42 +0800

    change kotlin to java

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,174 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/gallery_localizations.dart';
+import 'package:gallery/demos/material/material_demo_types.dart';
 
-void main(List<String> args) {
-  runApp(MyApp());
+class BottomNavigationDemo extends StatefulWidget {
+  const BottomNavigationDemo({
+    Key? key,
+    required this.restorationId,
+    required this.type,
+  }) : super(key: key);
+
+  final String restorationId;
+  final BottomNavigationDemoType type;
+
+  @override
+  State<BottomNavigationDemo> createState() => _BottomNavigationDemoState();
 }
 
-class MyApp extends StatelessWidget {
+class _BottomNavigationDemoState extends State<BottomNavigationDemo>
+    with RestorationMixin {
+  final RestorableInt _currentIndex = RestorableInt(0);
+
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+  String get restorationId => widget.restorationId;
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_currentIndex, 'bottom_navigation_tab_index');
   }
-}
 
-class HomePage extends StatelessWidget {
+  @override
+  void dispose() {
+    _currentIndex.dispose();
+    super.dispose();
+  }
+
+  String _title(BuildContext context) {
+    final localizations = GalleryLocalizations.of(context)!;
+    switch (widget.type) {
+      case BottomNavigationDemoType.withLabels:
+        return localizations.demoBottomNavigationPersistentLabels;
+      case BottomNavigationDemoType.withoutLabels:
+        return localizations.demoBottomNavigationSelectedLabel;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    final localizations = GalleryLocalizations.of(context)!;
+
+    var bottomNavigationBarItems = <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: const Icon(Icons.add_comment),
+        label: localizations.bottomNavigationCommentsTab,
+      ),
+      BottomNavigationBarItem(
+        icon: const Icon(Icons.calendar_today),
+        label: localizations.bottomNavigationCalendarTab,
+      ),
+      BottomNavigationBarItem(
+        icon: const Icon(Icons.account_circle),
+        label: localizations.bottomNavigationAccountTab,
+      ),
+      BottomNavigationBarItem(
+        icon: const Icon(Icons.alarm_on),
+        label: localizations.bottomNavigationAlarmTab,
+      ),
+      BottomNavigationBarItem(
+        icon: const Icon(Icons.camera_enhance),
+        label: localizations.bottomNavigationCameraTab,
+      ),
+    ];
+
+    if (widget.type == BottomNavigationDemoType.withLabels) {
+      bottomNavigationBarItems = bottomNavigationBarItems.sublist(
+          0, bottomNavigationBarItems.length - 2);
+      _currentIndex.value = _currentIndex.value
+          .clamp(0, bottomNavigationBarItems.length - 1)
+          .toInt();
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("图书馆管理系统"),
-        elevation: 1.5,
-        centerTitle: true,
+        automaticallyImplyLeading: false,
+        title: Text(_title(context)),
       ),
-      body: LoginPage(),
+      body: Center(
+        child: PageTransitionSwitcher(
+          transitionBuilder: (child, animation, secondaryAnimation) {
+            return FadeThroughTransition(
+              animation: animation,
+              secondaryAnimation: secondaryAnimation,
+              child: child,
+            );
+          },
+          child: _NavigationDestinationView(
+            // Adding [UniqueKey] to make sure the widget rebuilds when transitioning.
+            key: UniqueKey(),
+            item: bottomNavigationBarItems[_currentIndex.value],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels:
+            widget.type == BottomNavigationDemoType.withLabels,
+        items: bottomNavigationBarItems,
+        currentIndex: _currentIndex.value,
+        type: BottomNavigationBarType.fixed,
+        selectedFontSize: textTheme.caption!.fontSize!,
+        unselectedFontSize: textTheme.caption!.fontSize!,
+        onTap: (index) {
+          setState(() {
+            _currentIndex.value = index;
+          });
+        },
+        selectedItemColor: colorScheme.onPrimary,
+        unselectedItemColor: colorScheme.onPrimary.withOpacity(0.38),
+        backgroundColor: colorScheme.primary,
+      ),
     );
   }
 }
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class _NavigationDestinationView extends StatelessWidget {
+  const _NavigationDestinationView({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
 
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
+  final BottomNavigationBarItem item;
 
-class _LoginPageState extends State<LoginPage> {
-  List<String> continer = ["Hello world", "Goodbye World"];
-  int count = 0;
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        Text("$continer[count]"),
-        // RaisedButton(
-        //     onPressd: () {
-        //       setState(() {
-        //         count = ++count % 2;
-        //       });
-        //     },
-        //     child: Text("change"))
+        ExcludeSemantics(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.asset(
+                  'assets/demos/bottom_navigation_background.png',
+                  package: 'flutter_gallery_assets',
+                ),
+              ),
+            ),
+          ),
+        ),
+        Center(
+          child: IconTheme(
+            data: const IconThemeData(
+              color: Colors.white,
+              size: 80,
+            ),
+            child: Semantics(
+              label: GalleryLocalizations.of(context)!
+                  .bottomNavigationContentPlaceholder(
+                item.label!,
+              ),
+              child: item.icon,
+            ),
+          ),
+        ),
       ],
     );
   }
